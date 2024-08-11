3a7fceee88f3867c10642255818753e1159d5fc4
Clive Walkden <clive@sozodesign.co.uk>
2024-08-11 01:08:18 +0100

    chore: Add src/index.js to git add command in pre-commit hook

--- src/index.js
+++ src/index.js
@@ -1,28 +1,29 @@
 const core = require("@actions/core");
-const { Configuration, UptimeApi } = require('@statuscake/statuscake-js');
+const { Configuration, UptimeApi } = require("@statuscake/statuscake-js");
 
 async function run() {
-    try {
-        const monitorIds = core.getInput("statuscake_monitor_ids").split(",");
-        const action = core.getInput("action");
-        const apiToken = core.getInput("statuscake_api_token");
+  try {
+    const monitorIds = core.getInput("statuscake_monitor_ids").split(",");
+    const action = core.getInput("action");
+    const apiToken = core.getInput("statuscake_api_token");
 
-        const pause = action === "pause"; // Convert the action to a boolean
+    const pause = action === "pause"; // Convert the action to a boolean
 
-        const config = new Configuration({
-            headers: {
-                'Authorization': `Bearer ${apiToken}`,
-            },
-        });
-        const service = new UptimeApi(config);
+    const config = new Configuration({
+      headers: {
+        Authorization: `Bearer ${apiToken}`,
+      },
+    });
+    const service = new UptimeApi(config);
 
-        for (const monitorId of monitorIds) {
-            service.updateUptimeTest({testId: monitorId, paused: pause})
-                .then((tests) => core.info(JSON.stringify(tests)));
-        }
-    } catch (error) {
-        core.setFailed(`Action failed with error: ${error}`);
+    for (const monitorId of monitorIds) {
+      service
+        .updateUptimeTest({ testId: monitorId, paused: pause })
+        .then((tests) => core.info(JSON.stringify(tests)));
     }
+  } catch (error) {
+    core.setFailed(`Action failed with error: ${error}`);
+  }
 }
 
 run();
