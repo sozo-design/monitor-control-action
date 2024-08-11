e1febde0422587f0ad19062d109a7ae572e73a70
Clive Walkden <clive@sozodesign.co.uk>
2024-08-11 01:07:24 +0100

    chore: Update npm dependencies and add @statuscake/statuscake-js package

--- src/index.js
+++ src/index.js
@@ -1,46 +1,28 @@
 const core = require("@actions/core");
-const axios = require("axios");
-
-async function controlStatusCakeMonitor(monitorId, pause, apiToken) {
-  try {
-    const url = `https://api.statuscake.com/v1/uptime/${monitorId}`;
-    const data = { paused: pause }; // `pause` is a boolean
-    const headers = {
-      Authorization: `Bearer ${apiToken}`,
-      "Content-Type": "application/json",
-    };
-    core.info(
-      `Updating monitor ${monitorId} to ${pause ? "pause" : "restart"}`,
-    );
-    core.debug(`URL: ${url}`);
-    core.debug(`pause: ${pause}`);
-    core.debug(`data: ${JSON.stringify(data)}`);
-
-    const response = await axios.put(url, data, { headers });
-
-    core.info(`Monitor ${monitorId} updated successfully: ${response.data}`);
-  } catch (error) {
-    core.error(`Error updating monitor ${monitorId}: ${error}`);
-    throw error;
-  }
-}
+const { Configuration, UptimeApi } = require('@statuscake/statuscake-js');
 
 async function run() {
-  try {
-    const monitorIds = core.getInput("statuscake_monitor_ids").split(",");
-    const action = core.getInput("action");
-    const apiToken = core.getInput("statuscake_api_token");
+    try {
+        const monitorIds = core.getInput("statuscake_monitor_ids").split(",");
+        const action = core.getInput("action");
+        const apiToken = core.getInput("statuscake_api_token");
 
-    const pause = action === "pause"; // Convert the action to a boolean
+        const pause = action === "pause"; // Convert the action to a boolean
 
-    for (const monitorId of monitorIds) {
-      await controlStatusCakeMonitor(monitorId, pause, apiToken);
+        const config = new Configuration({
+            headers: {
+                'Authorization': `Bearer ${apiToken}`,
+            },
+        });
+        const service = new UptimeApi(config);
+
+        for (const monitorId of monitorIds) {
+            service.updateUptimeTest({testId: monitorId, paused: pause})
+                .then((tests) => core.info(JSON.stringify(tests)));
+        }
+    } catch (error) {
+        core.setFailed(`Action failed with error: ${error}`);
     }
-  } catch (error) {
-    core.setFailed(`Action failed with error: ${error}`);
-  }
 }
 
 run();
-
-exports.controlStatusCakeMonitor = controlStatusCakeMonitor;
